f2dc5b18b7121cceb28b8eeee43ecd09f0c43435
saidatla201 <saidatla201@gmail.com>
2023-02-09 12:12:50 +0530

    Welcome message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Linecomparision257
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparision Computation ");
+        }
+    }
+}
